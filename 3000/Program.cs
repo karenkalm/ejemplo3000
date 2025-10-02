44a9bba258974a112f9415f0cc92a864b7ff7072
karenkalm <klopez44@ucol.mx>
2025-10-02 15:39:39 -0600

    Agregar archivos de proyecto.

---
+++ 3000/Program.cs
@@ -0,0 +1,20 @@
+using _3000;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Registrar Blazorise (WebAssembly no necesita Use*)
+builder.Services
+    .AddBlazorise()
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
+
+await builder.Build().RunAsync();
